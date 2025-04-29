9a80f34cb10c1ea5083ce13ca5381b716f64b5b1
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-04-29 18:00:16 +0900

    fix : 가격 . -> ,으로 수정

--- src/utils/number.js
+++ src/utils/number.js
@@ -1,6 +1,6 @@
 export const currencyFormat = (value) => {
   const number = value !== undefined ? value : 0;
-  return number.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
+  return number.toFixed(0).replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1,");
 };
 
 export const cc_expires_format = (string) => {
