a9580f621a33782e6870431eedb59841f7071af2
xode114kr1 <144416796+xode114kr1@users.noreply.github.com>
2025-05-04 16:20:19 +0900

    feat : 주문 기능 추가 - 추가 시 카트 삭제 - 주문 완료 페이지로 이동

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -16,7 +16,17 @@ const initialState = {
 // Async thunks
 export const createOrder = createAsyncThunk(
   "order/createOrder",
-  async (payload, { dispatch, rejectWithValue }) => {}
+  async (payload, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("order", payload);
+      if (response.status !== 200) throw new Error(response.error);
+      dispatch(getCartQty());
+      return response.data.orderNum;
+    } catch (error) {
+      dispatch(showToastMessage({ message: error.error, status: "error" }));
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const getOrder = createAsyncThunk(
@@ -43,7 +53,21 @@ const orderSlice = createSlice({
       state.selectedOrder = action.payload;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = "";
+        state.orderNum = action.payload;
+      })
+      .addCase(createOrder.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { setSelectedOrder } = orderSlice.actions;
